8bb1b5ae5c6218f5cdf7dd86434d1ed1f4fe9d24
Ashdeuzo <hazem.mhiri@gmail.com>
2018-07-01 23:22:58 +0100

    static files caching

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,42 @@ import {
 	convertCurrencies
 } from "./components/currencies";
 
+const _registerServiceWorker = () => {
+	if ("serviceWorker" in navigator) {
+		navigator.serviceWorker.register("/js/sw.js").then(reg => {
+			if (!navigator.serviceWorker.controller) {
+				return;
+			}
+
+			if (reg.waiting) {
+				indexController._updateReady(reg.waiting);
+				return;
+			}
+
+			if (reg.installing) {
+				indexController._trackInstalling(reg.installing);
+				return;
+			}
+
+			reg.addEventListener("updatefound", () => {
+				indexController._trackInstalling(reg.installing);
+			});
+
+			let refreshing = false;
+			navigator.serviceWorker.addEventListener("controllerchange", () => {
+				if (refreshing) return;
+				window.location.reload();
+				refreshing = true;
+			});
+		});
+	}
+
+	return;
+};
+
 const main = () => {
+	_registerServiceWorker();
+
 	renderCurrenciesList();
 
 	document
